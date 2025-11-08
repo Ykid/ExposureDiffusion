0ade84125d20b0debdd590c7d9b1b1c36c8c04af
david <vagercai@gmail.com>
2025-11-08 19:37:02 +0000

    can run inference

---
+++ run_inference.sh
@@ -0,0 +1,7 @@
+python3 test_SID.py \
+    --model eld_iter_model \
+    --model_path "/home/david.weijiecai/computational_imaging/sid_PGru.pt" \
+    --include 4 --with_photon \
+    --adaptive_res_and_x0 \
+    -r --iter_num 2 \
+    --concat_origin
