6a2b801eeab8ce7c6d9a9aae4e1239c801794127
david <vagercai@gmail.com>
2025-11-12 22:03:11 +0000

    add requirements.txt and readme

--- run_inference.sh
+++ run_inference.sh
@@ -1,7 +1,14 @@
-python3 test_SID.py \
-    --model eld_iter_model \
-    --model_path "/home/david.weijiecai/computational_imaging/sid_PGru.pt" \
+# python3 test_SID.py \
+#     --model eld_iter_model \
+#     --model_path "/home/david.weijiecai/computational_imaging/sid_PGru.pt" \
+#     --include 4 --with_photon \
+#     --adaptive_res_and_x0 \
+#     -r --iter_num 2 \
+#     --concat_origin
+
+# iteration 2 is the best for naf2
+python3 test_SID.py --model eld_iter_model \
+    --model_path "/home/david.weijiecai/computational_imaging/sid_Pg_naf2.pt" \
     --include 4 --with_photon \
-    --adaptive_res_and_x0 \
-    -r --iter_num 2 \
-    --concat_origin
+    --adaptive_res_and_x0 -r \
+    --iter_num 2 --netG naf2
