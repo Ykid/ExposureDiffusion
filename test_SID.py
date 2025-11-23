85a3cc5885266d19464ce29f228cedff0f82df49
david <vagercai@gmail.com>
2025-11-23 15:28:38 +0000

    update test_SID.py to run only on the test set

--- test_SID.py
+++ test_SID.py
@@ -21,13 +21,14 @@ test_fns = dataset.read_paired_fns('./dataset/Sony_test.txt')
 
 eval_fns_list = [[fn for fn in eval_fns if min(int(read_expo_ratio(fn[1])/read_expo_ratio(fn[0])), 300)==ratio] for ratio in expo_ratio]
 test_fns_list = [[fn for fn in test_fns if min(int(read_expo_ratio(fn[1])/read_expo_ratio(fn[0])), 300)==ratio] for ratio in expo_ratio]
-eval_fns_list = [lst_1 + lst_2 for lst_1, lst_2 in zip(eval_fns_list, test_fns_list)]
-
+# eval_fns_list = [lst_1 + lst_2 for lst_1, lst_2 in zip(eval_fns_list, test_fns_list)]
+eval_fns_list = test_fns_list
+# import pdb; pdb.set_trace()
 # evaluate 15 indoor scenes (but you can also evaluate the performance on the whole dataset)
-indoor_ids = dataset.read_paired_fns('./SID_Sony_15_paired.txt')
+# indoor_ids = dataset.read_paired_fns('./SID_Sony_15_paired.txt')
 # indoor_ids = dataset.read_paired_fns('./SID_ablation.txt')
 # indoor_ids = dataset.read_paired_fns('./SID_Sony_paired.txt')
-eval_fns_list = [[(fn[0], fn[1]) for fn in indoor_ids if int(fn[2]) == ratio] for ratio in expo_ratio]
+# eval_fns_list = [[(fn[0], fn[1]) for fn in indoor_ids if int(fn[2]) == ratio] for ratio in expo_ratio]
 
 noise_model = noise.NoiseModel(model="P+g", include=4)
 eval_datasets = [datasets.SIDDataset(datadir, fns, noise_model, memorize=False, size=None, augment=False, stage_in=opt.stage_in, stage_out=opt.stage_out) for fns in eval_fns_list]
