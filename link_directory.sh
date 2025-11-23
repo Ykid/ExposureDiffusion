864ef3ccb1e43804c3838b654be83a4dce277144
david <vagercai@gmail.com>
2025-11-23 02:32:58 +0000

    add link directory script

---
+++ link_directory.sh
@@ -0,0 +1,5 @@
+ln -s /home/david.weijiecai/computational_imaging/new_testset_v2/test/blurred/  \
+    /home/david.weijiecai/computational_imaging/ExposureDiffusion/datasets/SID/Sony_blur_v2/short
+
+ln -s /home/david.weijiecai/computational_imaging/ExposureDiffusion/datasets/SID/Sony/long  \
+    /home/david.weijiecai/computational_imaging/ExposureDiffusion/datasets/SID/Sony_blur_v2/long
