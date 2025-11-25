2ab10280c1e0a9c608d067d92f6c35b63ae1f4ee
david <vagercai@gmail.com>
2025-11-25 21:52:25 +0000

    update run_training.sh

--- run_training.sh
+++ run_training.sh
@@ -32,20 +32,39 @@ fi
 # My favorite from the comments. Thanks @richarddewit & others!
 set -a && source .env_debug && set +a
 # overfitting
-python -m pdb train_syn.py \
+# python -m pdb train_syn.py \
+#         --name sid_Pg_naf2 \
+#         --include 4 \
+#         --noise P+g \
+#         --model eld_iter_model \
+#         --with_photon \
+#         --adaptive_res_and_x0 \
+#         --iter_num 2 \
+#         --epoch 300 \
+#         --max_dataset_size 1 \
+#         --auxloss \
+#         --continuous_noise \
+#         --adaptive_loss \
+#         --netG naf2 \
+#         --batchSize 1 \
+#         --nThreads 0
+
+python -m pdb train_syn_v2.py \
         --name sid_Pg_naf2 \
         --include 4 \
         --noise P+g \
-        --model eld_iter_model \
+        --model eld_iter_v2_model \
         --with_photon \
         --adaptive_res_and_x0 \
         --iter_num 2 \
-        --epoch 300 \
+        --epoch 600 \ # previously 300
         --max_dataset_size 1 \
         --auxloss \
         --continuous_noise \
         --adaptive_loss \
         --netG naf2 \
         --batchSize 1 \
-        --nThreads 0
-        
+        --nThreads 4 \
+        --resume \
+        --model_path /home/david.weijiecai/computational_imaging/sid_Pg_naf2.pt \
+        --checkpoints_dir /home/david.weijiecai/computational_imaging/ExposureDiffusion/checkpoints/
