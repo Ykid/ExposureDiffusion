48b370f3454814b6b8fef6dc98a1d1b5afa3d001
david <vagercai@gmail.com>
2025-11-25 20:26:56 +0000

    update init to use the new model

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,12 @@
 from .ELD_model import ELDModel
 from .ELD_model_iter import ELDModelIter
+from .ELD_model_iter_v2 import ELDModelIterV2
 
 def eld_model():
     return ELDModel()
 
 def eld_iter_model():
     return ELDModelIter()
+
+def eld_iter_v2_model():
+    return ELDModelIterV2()
