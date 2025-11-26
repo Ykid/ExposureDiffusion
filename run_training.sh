367d4bb9d35e7379f2c80dac671692e505e1150b
david <vagercai@gmail.com>
2025-11-26 02:33:02 +0000

    update training script

--- run_training.sh
+++ run_training.sh
@@ -1,11 +1,11 @@
-# # The initial version
-# if [ ! -f .env ]
-# then
-#   export $(cat .env | xargs)
-# fi
+# The initial version
+if [ ! -f .env ]
+then
+  export $(cat .env | xargs)
+fi
 
-# # My favorite from the comments. Thanks @richarddewit & others!
-# set -a && source .env && set +a
+# My favorite from the comments. Thanks @richarddewit & others!
+set -a && source .env && set +a
 
 # Training code
 # python train_syn.py \
@@ -21,16 +21,34 @@
 #         --continuous_noise \
 #         --adaptive_loss \
 #         --netG naf2 --batchSize 2 --nThreads 4
+# training with v2
+python -m pdb train_syn_v2.py \
+        --name sid_Pg_naf2 \
+        --include 4 \
+        --noise P+g \
+        --model eld_iter_v2_model \
+        --with_photon \
+        --adaptive_res_and_x0 \
+        --iter_num 2 \
+        --epoch 600 \
+        --auxloss \
+        --continuous_noise \
+        --adaptive_loss \
+        --netG naf2 \
+        --batchSize 1 \
+        --nThreads 4 \
+        --resume \
+        --model_path /home/david.weijiecai/computational_imaging/sid_Pg_naf2.pt \
+        --checkpoints_dir /home/david.weijiecai/computational_imaging/ExposureDiffusion/checkpoints/
 
+# # The initial version
+# if [ ! -f .env_debug ]
+# then
+#   export $(cat .env_debug | xargs)
+# fi
 
-# The initial version
-if [ ! -f .env_debug ]
-then
-  export $(cat .env_debug | xargs)
-fi
-
-# My favorite from the comments. Thanks @richarddewit & others!
-set -a && source .env_debug && set +a
+# # My favorite from the comments. Thanks @richarddewit & others!
+# set -a && source .env_debug && set +a
 # overfitting
 # python -m pdb train_syn.py \
 #         --name sid_Pg_naf2 \
@@ -49,22 +67,22 @@ set -a && source .env_debug && set +a
 #         --batchSize 1 \
 #         --nThreads 0
 
-python -m pdb train_syn_v2.py \
-        --name sid_Pg_naf2 \
-        --include 4 \
-        --noise P+g \
-        --model eld_iter_v2_model \
-        --with_photon \
-        --adaptive_res_and_x0 \
-        --iter_num 2 \
-        --epoch 600 \ # previously 300
-        --max_dataset_size 1 \
-        --auxloss \
-        --continuous_noise \
-        --adaptive_loss \
-        --netG naf2 \
-        --batchSize 1 \
-        --nThreads 4 \
-        --resume \
-        --model_path /home/david.weijiecai/computational_imaging/sid_Pg_naf2.pt \
-        --checkpoints_dir /home/david.weijiecai/computational_imaging/ExposureDiffusion/checkpoints/
+# just for debugging
+# python -m pdb train_syn_v2.py \
+#         --name sid_Pg_naf2 \
+#         --include 4 \
+#         --noise P+g \
+#         --model eld_iter_v2_model \
+#         --with_photon \
+#         --adaptive_res_and_x0 \
+#         --iter_num 2 \
+#         --epoch 600 \
+#         --auxloss \
+#         --continuous_noise \
+#         --adaptive_loss \
+#         --netG naf2 \
+#         --batchSize 1 \
+#         --nThreads 4 \
+#         --resume \
+#         --model_path /home/david.weijiecai/computational_imaging/sid_Pg_naf2.pt \
+#         --checkpoints_dir /home/david.weijiecai/computational_imaging/ExposureDiffusion/checkpoints_debug/
