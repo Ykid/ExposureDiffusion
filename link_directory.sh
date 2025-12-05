dd1fbee90bb2f9957e1fd98ba1471a32788f4226
david <vagercai@gmail.com>
2025-12-05 22:07:12 +0000

    remove redundant file

--- link_directory.sh
+++
@@ -1,8 +0,0 @@
-ln -s /home/david.weijiecai/computational_imaging/new_testset_v3/test_blur/blur  \
-    /home/david.weijiecai/computational_imaging/ExposureDiffusion/datasets/SID/Sony_blur_v3/short
-
-# ln -s /home/david.weijiecai/computational_imaging/ExposureDiffusion/datasets/SID/Sony/long  \
-    # /home/david.weijiecai/computational_imaging/ExposureDiffusion/datasets/SID/Sony_blur_v2/long
-
-ln -s /home/david.weijiecai/computational_imaging/ExposureDiffusion/datasets/SID/Sony/long  \
-    /home/david.weijiecai/computational_imaging/ExposureDiffusion/datasets/SID/Sony_blur_v3/long
